c928a770cd0d72cda404e9a5b290d2ab67eb4d16
gregbullington <gbullington23@gmail.com>
2022-09-26 21:17:02 -0700

    starting-build-out

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -3,5 +3,19 @@
 pragma solidity ^0.8.0;
 
 contract Lottery {
+    address payable[] public players;
+    address public manager;
 
-}
+    constructor () {
+        manager = msg.sender;
+    }
+
+    receive() external payable(
+        require(msg.value == 0.1 ether);
+        players.push(payable(msg.sender));
+    )
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+} 
